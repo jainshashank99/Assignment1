ec8505b5a655c10f0a372b991bbe448820920ce6
jainshashank99 <jainshashank99@gmail.com>
2012-09-12 11:32:37 +0530

    Comments added

--- CSP301_ass1/src/Political_blogs.java
+++ CSP301_ass1/src/Political_blogs.java
@@ -37,17 +37,18 @@ import prefuse.visual.expression.InGroupPredicate;
 
 public class Political_blogs {
 
-	private static Graph graph;
-	private static Visualization vis;
-	private static Display d;
+	private static Graph graph;//Graph in which the main data will be stored
+	private static Visualization vis;//visualization the created data
+	private static Display d;//display on which vis is added
 
 	public static void main(String[] args) throws IOException {
-		setUpData();
-		setUpVisualization();
-		setUpRenderers();
-		setUpActions();
-		setUpDisplay();
+		setUpData();//called to create graph by reading given file
+		setUpVisualization();//creating visualization of created graph
+		setUpRenderers();//render option for nodes
+		setUpActions();//coloring and layout actions performed
+		setUpDisplay();//Display created, vis added and controls added
 
+		//creating a frame on which display will be shown
 		JFrame frame = new JFrame("Political Books");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.add(d);
@@ -58,12 +59,14 @@ public class Political_blogs {
 	}
 
 	public static void setUpData() throws IOException {
+		//creating table in which graph will be stored 
 		graph = new Graph(true);
 		graph.addColumn("id", Integer.class);
 		graph.addColumn("label", String.class);
 		graph.addColumn("value", Integer.class);
 		graph.addColumn("source", String.class);
 
+		//reading file from polblogs using Filereader and Buffered Reader
 		FileInputStream fstream = new FileInputStream("polblogs.gml");
 		DataInputStream data = new DataInputStream(fstream);
 		BufferedReader br = new BufferedReader(new InputStreamReader(data));
@@ -71,6 +74,7 @@ public class Political_blogs {
 		br.readLine();
 		br.readLine();
 		String s = br.readLine();
+		//adding nodes
 		while (s.equals("  node [")) {
 			Node n = graph.addNode();
 			Scanner sc = new Scanner(br.readLine());
@@ -86,6 +90,7 @@ public class Political_blogs {
 			br.readLine();
 			s = br.readLine();
 		}
+		//adding edges
 		while (s.equals("  edge [")) {
 			Scanner sc = new Scanner(br.readLine());
 			sc.next();
@@ -108,6 +113,7 @@ public class Political_blogs {
 
 	// -- 3. the renderers and renderer factory ---------------------------
 	private static void setUpRenderers() {
+		//to give circular shape to the nodes
 		FinalRenderer r = new FinalRenderer();
 		DefaultRendererFactory drf = new DefaultRendererFactory(r);
 		vis.setRendererFactory(drf);
@@ -117,32 +123,41 @@ public class Political_blogs {
 	public static void setUpActions() {
 		
 		int[] palette = { ColorLib.rgb(255, 150, 150),ColorLib.rgb(150, 255, 150) };
+		//coloring on basis of whether the node is liberal or conservative
 		DataColorAction fill = new DataColorAction("graph.nodes", "value",Constants.NOMINAL, VisualItem.FILLCOLOR, palette);
+		//adding colors for highlighting
 		fill.add(VisualItem.FIXED, ColorLib.rgb(200,0,0));
         fill.add(VisualItem.HIGHLIGHT, ColorLib.rgb(255,200,125));
+        //coloring the edges
 		ColorAction edges = new ColorAction("graph.edges",VisualItem.STROKECOLOR, ColorLib.gray(200));
 		
+		//creating actionlist for adding color actions
 		ActionList color = new ActionList();
 		color.add(fill);
 		color.add(edges);
 		long b = 20000;
+		//creating action list for adding layout actions
 		ActionList layout = new ActionList(b);
+		//Force Directed layout given to Political blogs to see clustering
 		layout.add(new ForceDirectedLayout("graph", false));
 		layout.add(fill);
 		layout.add(new RepaintAction());
 		
+		//ading actions to visualisation
 		vis.putAction("color", color);
 		vis.putAction("layout", layout);
 		
 	}
 
 	public static void setUpDisplay() {
+		//vis added to display
 		d = new Display(vis);
-		d.setSize(1366, 768);
-		d.pan(550, 350);
+		d.setSize(1366, 768);//setting size
+		d.pan(550, 350);//making the origin of the graph somewhere in the middle of the screen
 		d.setForeground(Color.GRAY);
         d.setBackground(Color.WHITE);
 		
+        //different controls added to display
 		d.addControlListener(new DragControl());
 		d.addControlListener(new PanControl());
 		d.addControlListener(new ZoomControl());		
